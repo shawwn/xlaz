7caaf9ba5cbc206daa5b8b45508998eb98617a86
Shawn Presser <shawnpresser@gmail.com>
2022-02-04 22:27:28 -0600

    chore: add dependency tensorflow-checkpoint-reader

--- setup.py
+++ setup.py
@@ -43,10 +43,14 @@ packages = \
 package_data = \
 {'': ['*']}
 
+install_requires = \
+['tensorflow-checkpoint-reader>=0.1.0']
+
 setup_kwargs = {
     'package_dir': package_dir,
     'packages': packages,
     'package_data': package_data,
+    'install_requires': install_requires,
     'python_requires': '>=3.8,<4.0',
 }
 
